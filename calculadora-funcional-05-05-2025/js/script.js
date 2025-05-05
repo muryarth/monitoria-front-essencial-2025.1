e973e311e143981d1c729ec2857a8337e966b0b5
Arthur Mury <arthur.mury@outlook.com>
2025-05-05 00:08:59 -0300

    Subindo versão completa da calculadora funcional.

--- calculadora-funcional-05-05-2025/js/script.js
+++ calculadora-funcional-05-05-2025/js/script.js
@@ -1,5 +1,5 @@
 var painel = "";
-var calculo = { espera: "", resultado: "", operacao: "" };
+var calculo = { espera: "", operacao: "" };
 
 const numberButtons = document.querySelectorAll(".button-number");
 
@@ -16,7 +16,55 @@ const operationButtons = document.querySelectorAll(".button-operator");
 operationButtons.forEach(function (button) {
   button.addEventListener("click", function (event) {
     const tecla = event.target.textContent;
+
+    calculo.espera = painel || "0";
     calculo.operacao = tecla;
-    console.log(calculo.operacao);
+    painel = "";
+
+    document.getElementById(
+      "painel-espera"
+    ).textContent = `${calculo.espera} ${calculo.operacao}`;
+
+    document.getElementById("painel-calculo").textContent = "0";
   });
+});
+
+const resultButton = document.getElementById("button-result-calculator");
+
+resultButton.addEventListener("click", function () {
+  painel = calculaResultado(calculo.espera, painel, calculo.operacao);
+
+  calculo.espera = "";
+  calculo.operacao = "";
+
+  document.getElementById("painel-espera").textContent = "";
+  document.getElementById("painel-calculo").textContent = painel;
+});
+
+function calculaResultado(espera, valor, operacao) {
+  var resultado = 0;
+
+  switch (operacao) {
+    case "x":
+      resultado = parseFloat(espera) * parseFloat(valor);
+      break;
+    case "-":
+      resultado = parseFloat(espera) - parseFloat(valor);
+      break;
+    case "+":
+      resultado = parseFloat(espera) + parseFloat(valor);
+      break;
+  }
+
+  return resultado;
+}
+
+const resetButton = document.getElementById("button-reset");
+
+resetButton.addEventListener("click", function () {
+  calculo.espera = "";
+  calculo.operacao = "";
+  painel = "";
+  document.getElementById("painel-espera").textContent = "";
+  document.getElementById("painel-calculo").textContent = "0";
 });
